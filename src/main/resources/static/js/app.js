c1899be9eadb1089d153fc2755266e3ab4636813
D4v0r <davorcortes.dc@gmail.com>
2020-09-16 23:31:30 -0500

    Parte lll Terminada

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,33 @@
+app = ( () => {
+    let cinemaName = undefined;
+    let date = undefined;
+    let cinemaFunctions = [];
+
+    return{
+        setCinemaName(newName){
+            cinemaName = newName;
+        },
+
+        setDate(newDate){
+            date = newDate;
+        },
+
+        updateFunctions(cinemaName, functionDate){
+            apimock.getFunctionsByCinemaAndDate(cinemaName, functionDate,
+                    data => {
+               cinemaFunctions = data.map(({movie:{name: name, genre: genre}, date: date}) => (
+                    {
+                        name: name,
+                        genre:genre,
+                        hour: date.split(" ")[1],
+                    })
+                );
+               $("#functionsTable").empty();
+                 cinemaFunctions.forEach(o => {
+                     const {name, genre, hour} = o;
+                     $("#functionsTable").append(`<tr><td>${name}</td><td>${genre}</td><td>${hour}</td><td>open</td>></tr>`);
+                    });
+            })
+        }
+    }
+})();
